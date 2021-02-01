24949d1b8e3bce3aa37bdb5e67d14bd52fe9768a
Shwetha <Shwetha@DESKTOP-EM8SP10>
2021-02-01 10:21:58 +0530

    first commit

---
+++ Maven/src/test/java/com/actitime/test/DemoMaven.java
@@ -0,0 +1,11 @@
+package com.actitime.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+@Test
+public void CreateCustomer() {
+Reporter.log("CreateCustomer",true);
+}
+}
